14df092e8daa4e4318d56fcc85cbd62f7995c81c
BLADWING <timoti.ponarenkO@gmail.com>
2022-07-07 01:17:12 +0400

    add progress and Loader Spiner

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-
-
+import { Rings } from "react-loader-spinner";
+import { Progress } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Quiz() {
   return (
     <div>
       <h1>This is Quiz...</h1>
+      <Rings />
+
+      <Progress multi>
+        <Progress bar color="success" value="30" />
+      </Progress>
     </div>
   );
 }
