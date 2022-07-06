2073e094ccc22fb5cdd92321144f5f452445c4f0
BLADWING <timoti.ponarenkO@gmail.com>
2022-07-07 01:39:17 +0400

    create map for questions(not work)

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Rings } from "react-loader-spinner";
 import { Progress } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ApiQuizDB } from "../connectors/ApiConector";
 
-export default function Quiz() {
+export default function Quiz(props) {
+  const [isCorrect, setIsCorrect] = useState(null);
+  const [selected, setSelected] = useState(null);
+  const [confirm, setConfirm] = useState(false);
+
+  const selectAnswer = (id) => {
+    if (!confirm) {
+      setSelected(id + 1);
+    }
+  };
+
   return (
     <div>
       <h1>This is Quiz...</h1>
+
+      {props.question.options.map((option, index) => (
+        <div
+          key={option}
+          className={
+            "single-option " +
+            (isCorrect === true && selected - 1 === index
+              ? "correct"
+              : isCorrect === false && selected - 1 === index
+              ? "wrong"
+              : selected - 1 === index
+              ? "active "
+              : "")
+          }
+          onClick={() => selectAnswer(index)}
+        >
+          <span>{option}</span>
+        </div>
+      ))}
+
       <Rings />
 
       <Progress multi>
