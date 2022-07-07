4ec1362359bbe240a9fc5ed1bd9383e514980eb6
Tim Ponarenko <user.name>
2022-07-07 12:28:16 +0400

    renew and fix API

--- src/connectors/ApiConector.jsx
+++ src/connectors/ApiConector.jsx
@@ -1,23 +1,8 @@
-import { useEffect, useState } from "react";
-
-function useFetch(url) {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => {
-        setData(json);
-      });
-  }, [url]);
+const url = "https://my-json-server.typicode.com/bladwing/DB/db";
+export const questionData = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
   return data;
-}
-
-export const ApiQuizDB = () => {
-    const url = "https://my-json-server.typicode.com/bladwing/DB/db" ;
-  
-    const data = useFetch(url);
-  
-    return { data };
-  };
-
+};
 
