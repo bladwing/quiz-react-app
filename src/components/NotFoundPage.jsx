c84385e5075f010111fddedaa83907a5f803d223
BLADWING <timoti.ponarenkO@gmail.com>
2022-08-21 23:17:20 +0400

    create notFoundPage 404 #2

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import NotFoundPhoto from "../assets/img/404.png";
 
 export default function NotFoundPage() {
   return (
     <div className="notFoundPageContainer">
-        <img src="./404.png" alt="404" />
+        <img src={NotFoundPhoto} alt="404" />
       <h1>Page not found</h1>
       <h2>
         Please back to <Link to="/">Main page</Link>
